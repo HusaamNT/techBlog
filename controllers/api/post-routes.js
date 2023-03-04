759387a4e9a3e1a4e63e722edb6d3db06aadf1c3
Husaam NT <husnaeem@gmail.com>
2023-03-04 13:31:01 +0000

    added find all and by id post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,29 @@
 const router = require("express").Router();
-const {Posts} = require("../../models/Accounts");
-const {v4: uuid} = require("uuid");
+const { Posts } = require("../../models/Accounts");
+const { v4: uuid } = require("uuid");
 const bcrypt = require("bcrypt");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Posts.findAll();
+    console.log(postData);
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Posts.findByPk();
+    if (!postData) {
+      {
+        res.status(404).json({ message: "Post not found" });
+      }
+      res.status(200).json(postData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+    
