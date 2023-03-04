23d9d38d0bc2e5a05d61d45f06df620569f75769
Husaam NT <husnaeem@gmail.com>
2023-03-04 14:38:13 +0000

    added home route for webpage and handlebars syntax for main.html, completed seeds.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 const { Account, Post } = require('../models')
 
+router.get('/', async (req, res) => {
+    try {console.log(req.session.username)
+        res.render('all', {
+            title: 'Home',
+            message: 'Welcome to my website!',
+            username: req.session.username
+        });    
+    } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+
+module.exports = router;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -8,7 +8,7 @@ const seedAll = async () => {
     await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   
-  await seedAccounts();
+  await seedAccount();
   console.log('\n----- ACCOUNTS SEEDED -----\n');
 
   await seedPost();
