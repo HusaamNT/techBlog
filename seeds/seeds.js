5baad2c919feb8d599bccf1709cef23be8f8fbac
Husaam NT <husnaeem@gmail.com>
2023-03-04 14:02:26 +0000

    added seedAll function

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,11 +1,18 @@
-const Posts = require("../models/Posts");
+const seedAccount = require("./accounts");
+const seedPost = require("./posts")
 
-const postsData = [
-{
-    
-}
-];
+const sequelize = require('../config/connection');
 
-const seedPost = () => Posts.bulkCreate(postsData);
+const seedAll = async () => {
 
-module.exports = seedPost;
+    await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  
+  await seedAccounts();
+  console.log('\n----- ACCOUNTS SEEDED -----\n');
+
+  await seedPost();
+  console.log('\n----- POST SEEDED -----\n');
+};
+
+module.exports = seedAll();
